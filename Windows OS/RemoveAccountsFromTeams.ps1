b302795f180430083fca15e1229576e30ef6a6bf
notfocust <57000901+notfocust@users.noreply.github.com>
2025-12-17 10:26:14 +0100

    Add files via upload

---
+++ Windows OS/RemoveAccountsFromTeams.ps1
@@ -0,0 +1,18 @@
+﻿#removeteamsaccounts.ps1
+
+# Ensure the script is running as Administrator
+$IsAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $IsAdmin) {
+    Write-Host "Restarting script as Administrator..."
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+# Stop Teams-related processes
+taskkill /im ms-teams.exe /f
+
+# Remove Teams application data
+Remove-Item -Path "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe" -Recurse -Force
+
+# Start Teams to recreate necessary files
+Start "ms-teams.exe"
